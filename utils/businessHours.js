b15bbc3289152318b9afce958eee9724f0cbd053
Joé Jemmely <joe.jemmely@gmail.com>
2019-07-15 11:55:04 -0700

    add businessHours constant

---
+++ utils/businessHours.js
@@ -0,0 +1,15 @@
+export const businessHours = [
+  { id: 0, time: 9, label: '9:00 AM' },
+  { id: 1, time: 10, label: '10:00 AM' },
+  { id: 2, time: 11, label: '11:00 AM' },
+  { id: 3, time: 12, label: '12:00 PM' },
+  { id: 4, time: 13, label: '1:00 PM' },
+  { id: 5, time: 14, label: '2:00 PM' },
+  { id: 6, time: 15, label: '3:00 PM' },
+  { id: 7, time: 16, label: '4:00 PM' },
+  { id: 8, time: 17, label: '5:00 PM' },
+  { id: 9, time: 18, label: '6:00 PM' },
+  { id: 10, time: 19, label: '7:00 PM' },
+  { id: 11, time: 20, label: '8:00 PM' },
+  { id: 12, time: 21, label: '9:00 PM' }
+];
