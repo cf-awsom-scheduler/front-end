f0eda31768f80a124864f22403373ac5155c4ead
Joé Jemmely <joe.jemmely@gmail.com>
2019-07-18 10:02:26 -0700

    Cleanup nav

--- components/nav.js
+++ components/nav.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 
-const links = [
-  { href: 'https://github.com/segmentio/create-next-app', label: 'Github' }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`;
-  return link;
-});
-
 const Nav = ({ user }) => (
   <nav>
     <ul>
@@ -42,16 +35,6 @@ const Nav = ({ user }) => (
           </Link>
         </li>
       )}
-
-      <ul>
-        {links.map(({ key, href, label }) => (
-          <li key={key}>
-            <Link href={href}>
-              <a>{label}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
     </ul>
 
     <style jsx>{`
