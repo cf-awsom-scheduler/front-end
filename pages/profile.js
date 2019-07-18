58f877554ec27c47df410aebf8e21be8cbc9cc3d
Joé Jemmely <joe.jemmely@gmail.com>
2019-07-17 17:13:44 -0700

    Use Auth0

--- pages/profile.js
+++ pages/profile.js
@@ -1,70 +1,20 @@
-import React from 'react';
-import Router from 'next/router';
-import fetch from 'isomorphic-unfetch';
-import nextCookie from 'next-cookies';
-import { withAuthSync } from '../utils/auth';
-
-const Profile = props => {
-  const { name, login, bio, avatarUrl } = props.data;
-
+function Profile({ user }) {
   return (
-    <>
-      <img src={avatarUrl} alt="Avatar" />
-      <h1>{name}</h1>
-      <p className="lead">{login}</p>
-      <p>{bio}</p>
-
-      <style jsx>{`
-        img {
-          max-width: 200px;
-          border-radius: 0.5rem;
-        }
-        h1 {
-          margin-bottom: 0;
-        }
-        .lead {
-          margin-top: 0;
-          font-size: 1.5rem;
-          font-weight: 300;
-          color: #666;
-        }
-        p {
-          color: #6a737d;
-        }
-      `}</style>
-    </>
+    <div>
+      <h2>
+        <div src={user.picture} alt={user.displayName} /> Hello,{' '}
+        {user.displayName}
+      </h2>
+      <p>This is what we know about you:</p>
+      <ul>
+        {Object.keys(user).map(key => (
+          <li key={key}>
+            {key}: {user[key].toString()}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-};
+}
 
-Profile.getInitialProps = async ctx => {
-  const { token } = nextCookie(ctx);
-  const apiUrl = '/api/profile';
-
-  const redirectOnError = () =>
-    typeof window !== 'undefined'
-      ? Router.push('/login')
-      : ctx.res.writeHead(302, { Location: '/login' });
-
-  try {
-    const response = await fetch(apiUrl, {
-      credentials: 'include',
-      headers: {
-        Authorization: JSON.stringify({ token })
-      }
-    });
-
-    if (response.ok) {
-      const js = await response.json();
-      console.log('js', js);
-      return js;
-    } else {
-      // https://github.com/developit/unfetch#caveats
-      return await redirectOnError();
-    }
-  } catch (error) {
-    // Implementation or Network error
-    return redirectOnError();
-  }
-};
-
-export default withAuthSync(Profile);
+export default Profile;
